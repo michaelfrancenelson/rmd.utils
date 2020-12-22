d80e871eaea6736febe4bc795624d88ccddddbbd
Michael France Nelson <michaelfrancenelson@gmail.com>
2020-12-22 17:25:37 -0500

    adding markup getter for questions

---
+++ R/get_question_markup.R
@@ -0,0 +1,33 @@
+#'
+#'
+#' @export
+
+get_question_markup = function(
+  q_files,
+  q_fmt = "## Question %s",
+  q_header = "Question\\s{0,}$",
+  delim = "^={3,}\\s{0,}$",
+  a_list_fmt = "answerlist")
+{
+  q_text = unlist(
+    lapply(
+      q_files,
+      function(x) rmd.utils::get_question_section(
+        f_lines = readLines(x))))
+
+  grep(pattern = delim, x = q_text)
+
+  q_text = gsub(
+    pattern = delim, replacement = "", x = q_text)
+
+  q_lines = grep(q_header, x = q_text)
+
+  q_text[q_lines] = sprintf(q_fmt, 1:length(q_files))
+
+  a_list_lines = grep(
+    pattern = a_list_fmt, x = q_text)
+
+  q_text = q_text[-a_list_lines]
+
+  return(trimws(q_text))
+}
